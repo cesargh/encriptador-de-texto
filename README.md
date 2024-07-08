8d47f092e39029ca4b3a10501ff880470c7cb6ed
cesargh <cesar.joblink@gmail.com>
2024-07-07 22:58:53 -0300

    Aplicación finalizada

--- README.md
+++ README.md
@@ -4,22 +4,34 @@ Encriptador de Texto es una aplicación desarrollada para el Challenge del curso
 
 ## Modo de Uso
 
-1) Visualiza el área de entrada de texto con la leyenda "Ingresa aquí el texto a encriptar".
+1) Visualiza el área de entrada de texto con la leyenda "Ingresa el texto aquí".
 2) Escribe o pega en ella el texto que desees. Ten en cuenta que sólo puedes usar letras minúsculas sin acento.
-3) Visualiza el área de salida de texto con la leyenda "Aquí verás el texto encriptado". Presiona el botón "Encriptar".
-4) Si la encriptación resultare exitosa, verás el resultado. De lo contrario, el mensaje de error correspondiente.
-5) Si deseas desencriptar, repite los pasos 1 a 4, pero ingresando un texto encriptado válido y presionando el botón "Desencriptar".
-6) Puedes presionar el botón "Copiar" para almacenar el texto encriptado en el portapapeles.
-7) Puedes presionar el botón "Pegar" para ingresar desde el portapapeles el texto que hubieres almacenado con anterioridad.
+3) A continuación, presiona el botón "Encriptar".
+4) Si la encriptación fuera exitosa, verás el resultado. De lo contrario, verás el mensaje de error correspondiente.
+5) Si deseas desencriptar, repite los pasos 1 a 4, pero ingresando un texto encriptado y presionando el botón "Desencriptar".
+6) Puedes presionar el botón "Copiar" para almacenar el texto resultante en el portapapeles.
+7) Puedes presionar el botón "Pegar" para ingresar desde el portapapeles cualquier texto que almacenaras con anterioridad.
 
 ## Algoritmo de Encriptación
 
-* La letra "a" es convertida en "ai".
-* La letra "e" es convertida en "enter".
-* La letra "i" es convertida en "imes".
-* La letra "o" es convertida en "ober".
-* La letra "u" es convertida en "ufat".
+La encriptación sustituye las vocales por una cadena de caracteres específica, respetando el siguiente orden de reemplazo:
+
+1) La letra "e" es convertida en "enter".
+2) La letra "i" es convertida en "imes".
+3) La letra "a" es convertida en "ai".
+4) La letra "o" es convertida en "ober".
+5) La letra "u" es convertida en "ufat".
+
+La desencriptación realiza el mismo procedimiento, pero en orden inverso.
+
+## Agradecimientos
+
+* https://openai.com/ (Inteligencia artificial)
+* https://www.klipartz.com/ (Imágenes gratuitas o con licencia de uso no comercial)
+* https://www.flaticon.com/ (Íconos gratuitos o con licencia de uso no comercial)
+* https://onlinepngtools.com/ (Herramientas para edición de archivos PNG)
+* https://favicon.io/ (Generador de favicon)
 
 ## Acerca del Autor
 
-César es alumno de Alura. Inició su formación en Junio de 2024 dentro del plan Oracle Next Education para latinoamérica. Tiene 54 años y es nacido en Buenos Aires.
+César es alumno de Alura. Inició su formación en Junio de 2024 dentro del plan Oracle Next Education para Latinoamérica. Tiene 54 años y es nacido en Buenos Aires.
--- app.js
+++ app.js
@@ -6,22 +6,124 @@ License: None (Public Domain)
 
 */
 
-function onclickButtonEncrypt() {
-    // TO-DO: Procedimiento para Encriptar
-    alert('Encriptar');
+const encryptionKeys = [
+    { char: 'e', word: 'enter' },
+    { char: 'i', word: 'imes' },
+    { char: 'a', word: 'ai' },
+    { char: 'o', word: 'ober' },
+    { char: 'u', word: 'ufat' }
+];
+
+const taInput = document.getElementById("textareaInput");
+const taOutput = document.getElementById("textareaOutput");
+
+const pgStatusInput = document.getElementById("paragraphStatusInput");
+const pgStatusOutput = document.getElementById("paragraphStatusOutput");
+const pgStatusBlank = "\u00A0";
+
+let callbackTimeout = null;
+
+function isEmptyText(text) {
+    return (text.trim().length === 0);
 }
 
-function onclickButtonDecrypt() {
-    // TO-DO: Procedimiento para Desencriptar    
-    alert('Desencriptar');
+function isValidText(text) {
+    const restriction = /^[a-zñç ]+$/;
+    return restriction.test(text);;
 }
 
-function onclickButtonPaste() {
-    // TO-DO: Procedimiento para Pegar    
-    alert('Pegar');
+function replaceCharacters(sortForward, inputText) {
+    const sortedKeys = sortForward ? encryptionKeys : encryptionKeys.slice().reverse();
+    let outputText = inputText;
+    sortedKeys.forEach(({ char, word }) => {
+        const searchKey = sortForward ? char : word;
+        const replaceKey = sortForward ? word : char;
+        const searchRule = new RegExp(searchKey, 'g');
+        outputText = outputText.replace(searchRule, replaceKey);
+    });
+    return outputText;
 }
 
-function onclickButtonCopy() {
-    // TO-DO: Procedimiento para Copiar        
-    alert('Copiar');
+function delayCallback(callbackFunction) {
+    clearTimeout(callbackTimeout);
+    callbackTimeout = setTimeout(callbackFunction, 500);
+}
+  
+function synchTextareaOutput(isWriting) {
+    taOutput.value = "";
+    taOutput.className = "textarea__default textarea__output__" + (isWriting ? "writing" : "waiting");
+    pgStatusOutput.innerText = pgStatusBlank;
+    if (isWriting) {
+        pgStatusInput.innerText = "Sólo letras minúsculas sin acentos.";
+    }
+}
+
+function onclickButtonEncrypt(sortForward) {
+    let outputText = "";
+    try {
+        const inputText = taInput.value;
+        if (isEmptyText(inputText)) {
+            pgStatusInput.innerText = "No hay texto que procesar...";
+        }
+        else if (! isValidText(inputText)) {
+            pgStatusInput.innerText = "Texto inválido. Sólo se aceptan letras minúsculas sin acentos.";
+        }
+        else {
+            outputText = replaceCharacters(sortForward, inputText);
+        }
+    }
+    catch(excep) {
+        console.log(excep);
+        pgStatusInput.innerText = `Error al ${sortForward ? "encriptar" : "desencriptar"}. Detalles en la consola.`;
+    }
+    if (isEmptyText(outputText)) {
+        synchTextareaOutput(false);
+    }
+    else {
+        taOutput.value = outputText;
+        taOutput.className = "textarea__default";
+        pgStatusOutput.innerText = pgStatusBlank;
+        pgStatusInput.innerText = `Texto ${sortForward ? "encriptado" : "desencriptado"} con éxito.`;
+    }
+}
+
+async function onclickButtonPaste() {
+    synchTextareaOutput(false);
+    try {
+        const clipText = await navigator.clipboard.readText();
+        if (typeof clipText === "string") {
+            if (isEmptyText(clipText)) {
+                pgStatusInput.innerText = "Nada que pegar...";
+            }
+            else {
+                taInput.value = clipText;
+                pgStatusInput.innerText = "Texto pegado desde el portapapeles.";
+            }
+        }
+        else {
+            pgStatusInput.innerText = "El portapapeles no contiene un texto.";
+        }
+    }
+    catch(excep) {
+        console.log(excep);
+        pgStatusInput.innerText = "Error al pegar. Detalles en la consola.";
+    }
+}
+
+async function onclickButtonCopy() {
+    pgStatusInput.innerText = pgStatusBlank;
+    try {
+        const outputText = taOutput.value;
+        if (isEmptyText(outputText)) {
+            pgStatusOutput.innerText = "Nada que copiar...";
+        } 
+        else {
+            await navigator.clipboard.writeText(outputText);
+            pgStatusOutput.innerText = "Texto copiado al portapapeles.";  
+        }
+    }
+    catch(excep) {
+        console.log(excep);
+        pgStatusOutput.innerText = "Error al copiar. Detalles en la consola.";
+    }
 }
